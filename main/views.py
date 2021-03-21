2162a41aa8f51710364f214694b5ebb9a6a5bd23
ravinalawade <ravinalawade5@gmail.com>
2021-03-21 13:51:20 +0530

    checking

--- main/views.py
+++ main/views.py
@@ -629,8 +629,8 @@ class Login_api(APIView):
             content = {'message': 'Logged in ','token':token.key,'email':True,'password':True,'password_registration':True,'user':dic}
         else:
             # Return an 'invalid login' error message.
-            u=User.objects.get(username='forestweb')
-            if u.password == username:
+            u=User.objects.get(username=username)
+            if u.password == 'forestweb':
                 content = {'message': 'Please register','email':True,'password':False,'password_registration':False}
             else:
                 content = {'message': 'Not register','email':False,'password':False,'password_registration':False}
@@ -706,8 +706,8 @@ class Check(APIView):
     def post(self,request):
         print("checking email id",request.data['email'])
         try:
-            u=User.objects.get(username='forestweb')
-            if u.password == request.data['email']:
+            u=User.objects.get(username=request.data['email'])
+            if u.password == 'forestweb':
                 flag=3
             else:
                 flag=2
