15eb4c7ab70706f045f4584afb3277678f505c4f
ravinalawade <ravinalawade5@gmail.com>
2021-03-31 00:11:06 +0530

    small mistake

--- main/routing.py
+++ main/routing.py
@@ -5,6 +5,6 @@ websocket_urlpatterns = [
     # re_path(r'ws/chat/(?P<friendship_id>\w+)/$', consumers.ChatConsumer.as_asgi()),
     path('web_socket', consumers.Web_socket.as_asgi(), name='web_socket'),
     path('animal_socket', consumers.Animal_socket.as_asgi(), name='animal_socket'),
-    path('animallist_socket', consumers.Animallis_socket.as_asgi(), name='animallist_socket')
+    path('animallist_socket', consumers.Animallist_socket.as_asgi(), name='animallist_socket')
     # path('alertnotify', consumers.Alertnotify.as_asgi(), name='alertnotify'),
 ]
